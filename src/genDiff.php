d7ddff2dc343638e841efee2da665d85a1ec086b
Turbanov Valeriy <tur.4000@gmail.com>
2021-02-01 01:05:36 +0200

    generate plain diff

--- src/genDiff.php
+++ src/genDiff.php
@@ -10,6 +10,60 @@ function genDiff(string $file1, string $file2, string $format = 'stylish')
     $data1 = json_decode($firstFile, true);
     $data2 = json_decode($secondFile, true);
 
-    var_dump($data1);
-    var_dump($data2);
+    $ast = buildAst($data1, $data2);
+
+    $diff = buildDiff($ast);
+
+    return $diff;
+}
+
+function buildAst($data1, $data2)
+{
+    $ast = [];
+
+    foreach ($data1 as $key => $value) {
+        if (!array_key_exists($key, $data2)) {
+            $ast[$key] = ['deleted', [$value]];
+        } elseif ($value === $data2[$key]) {
+            $ast[$key] = ['unchanged', [$value]];
+        } elseif ($value !== $data2[$key]) {
+            $ast[$key] = ['modified', [$value, $data2[$key]]];
+        }
+    }
+
+    foreach ($data2 as $key => $value) {
+        if (!array_key_exists($key, $data1)) {
+            $ast[$key] = ['added', [$value]];
+        }
+    }
+
+    ksort($ast);
+
+    return $ast;
+}
+
+function buildDiff($ast)
+{
+    $result = '{' . PHP_EOL;
+    foreach ($ast as $key => $value) {
+        switch ($value[0]) {
+            case 'unchanged':
+                $result .= "    {$key}: {$value[1][0]}" . PHP_EOL;
+                break;
+            case 'modified':
+                $result .= "  - {$key}: {$value[1][0]}" . PHP_EOL;
+                $result .= "  + {$key}: {$value[1][1]}" . PHP_EOL;
+                break;
+            case 'added':
+                $result .= "  + {$key}: {$value[1][0]}" . PHP_EOL;
+                break;
+            case 'deleted':
+                $result .= "  - {$key}: {$value[1][0]}" . PHP_EOL;
+                break;
+        }
+    }
+
+    $result .= '}';
+
+    return $result;
 }
